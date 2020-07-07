e948590b12ff1897e6d4d1a418cf03d80aa014eb
Diana Telahun <telahun.diana@gmail.com>
2020-07-07 10:45:19 -0700

    undo login

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -5,15 +5,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MovieService {
-  url: string =
-    'https://api.themoviedb.org/3/trending/all/day?api_key=f65b258bd7ca7a4b4fc13c07dfe75152';
+  // url: string =
+  //   'https://api.themoviedb.org/3/trending/all/day?api_key=f65b258bd7ca7a4b4fc13c07dfe75152';
 
-  base_url = 'https://api.themoviedb.org/3/movie/';
+  // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+
+  base_url = 'https://api.themoviedb.org/3/';
 
   api_key = 'f65b258bd7ca7a4b4fc13c07dfe75152';
 
   constructor(private http: HttpClient) {}
+  // get POPULAR Movies
   getMovies() {
-    return this.http.get(this.url);
+    return this.http.get(
+      `${this.base_url}movie/popular?api_key=${this.api_key}`
+    );
+  }
+  // get movie Genres
+  // https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
+  getGenres() {
+    return this.http.get(
+      `${this.base_url}genre/movie/list?api_key=${this.api_key}`
+    );
   }
 }
